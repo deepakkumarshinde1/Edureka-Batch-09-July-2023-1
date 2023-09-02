f8fb9c7f278370f2a1459bd4a0e82fb15209c571
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-09-02 18:52:23 +0530

    Javascript Basic-2

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,44 +1,4 @@
-let a = 20;
-let b = 30;
+// array ==> store data
+// store same or different type of data
 
-// arithmetic operator's
-let result = a + b;
-// console.log(result);
-
-result = a - b;
-// console.log(result);
-
-result = a * b;
-// console.log(result);
-
-result = a / b;
-// console.log(result);
-
-result = a % b; // modulus (reminder)
-// console.log(result);
-
-// assignment operator's
-let a1 = 10;
-a1 += 10; // a1 = a1 + 10;
-a1 -= 10; // a1 = a1 - 10;
-a1 *= 10; // a1 = a1 * 10;
-a1 /= 10; // a1 = a1 / 10;
-a1 %= 10; // a1 = a1 % 10;
-
-let a2 = 10;
-// a2 = a2 + 1;
-// a2 += 1
-
-// inc operator
-a2++; // post inc
-++a2; // pre inc
-
-// dec operator
-a2--; // port dec
---a2; // pre dec
-
-let a3 = 10;
-let b3 = ++a3;
-
-console.log("b3", b3);
-console.log("a3", a3);
+let array = [];
